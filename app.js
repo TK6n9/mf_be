32020387ebafbbda799cb591d9049c6340e28d14
TK6n9 <1tw9@naver.com>
2023-11-22 11:22:45 +0900

    레디스다시수정...2

--- app.js
+++ app.js
@@ -16,12 +16,26 @@ const socketIo = require("socket.io");
 const cors = require("cors");
 dotenv.config();
 
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   password: process.env.REDIS_PASSWORD,
+//   logErrors: true,
+// });
+
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-  logErrors: true,
+  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+  legacyMode: true,
 });
+
+redisClient.on("connect", () => {
+  console.info("Redis connected!");
+});
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+redisClient.connect().then(); // redis v4 연결 (비동기)
+const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
 
 const options = {
   key: fs.readFileSync("./rootca.key"),
@@ -100,7 +114,7 @@ app.use(
       httpOnly: true,
       secure: false,
     },
-    store: new RedisStore({ client: redisClient }),
+    store: new RedisStore({ client: redisClient, prefix: "session:" }),
   })
 );
 
